4a58c9d328bd3178a0bb51d3f65b3a04fcb79f6f
Aghna SJ <santiagoaghnajunior2003@gmail.com>
2023-06-11 22:47:43 +0700

    commit3

--- GUI_PU1Coba.py
+++ GUI_PU1Coba.py
@@ -2,17 +2,31 @@ from tkinter import Tk, Label, Entry, Button, StringVar, messagebox
 from PIL import ImageTk, Image
 from tkinter import LabelFrame
 
+def load_accounts():
+    # Fungsi ini akan dipanggil saat aplikasi dimulai untuk memuat akun-akun yang sudah ada dari file txt
+    with open("akun.txt", "r") as file:
+        lines = file.readlines()
+        for line in lines:
+            username, password = line.strip().split(":")
+            accounts[username] = password
+
+def save_accounts():
+    # Fungsi ini akan dipanggil saat ada akun baru yang dibuat untuk menyimpan semua akun ke dalam file txt
+    with open("akun.txt", "w") as file:
+        for username, password in accounts.items():
+            file.write(f"{username}:{password}\n")
+
 def login():
     # Fungsi ini akan dipanggil saat tombol Login ditekan
     username = username_entry.get()
     password = password_entry.get()
 
     if username == "" or password == "":
-        messagebox.showwarning("Error", "Buat Username dan Password Dahulu Sebelum Login!")
+        messagebox.showwarning("Error", "Username and password are required!")
     elif username in accounts and accounts[username] == password:
         open_biodata_menu()
     else:
-        messagebox.showerror("Error", "Username atau Password yang Anda Inputkan Salah atau Belum Tersedia!")
+        messagebox.showerror("Error", "Invalid username or password!")
 
 def create_account():
     # Fungsi ini akan dipanggil saat tombol Create Account ditekan
@@ -20,12 +34,13 @@ def create_account():
     password = password_entry.get()
 
     if username == "" or password == "":
-        messagebox.showwarning("Error", "Username Beserta Password Belum di Inputkan!")
+        messagebox.showwarning("Error", "Username and password are required!")
     elif username in accounts:
-        messagebox.showerror("Error", "Username dan Password yang Anda Inputkan Sudah Ada!")
+        messagebox.showerror("Error", "Username already exists!")
     else:
         accounts[username] = password
-        messagebox.showinfo("Success", "Akun Untuk Login Berhasil Dibuat!")
+        save_accounts()  # Menyimpan akun yang baru dibuat
+        messagebox.showinfo("Success", "Account created successfully!")
 
 def open_biodata_menu():
     # Fungsi ini akan dipanggil saat login berhasil dan membuka menu biodata
@@ -34,14 +49,14 @@ def open_biodata_menu():
 
 def save_biodata():
     # Fungsi ini akan dipanggil saat tombol Save ditekan pada menu biodata
-    Nama_Pemesan = name_entry.get()
+    name = name_entry.get()
     phone = phone_entry.get()
 
-    if Nama_Pemesan == "" or phone == "":
-        messagebox.showwarning("Error", "Nama Pemesan atau Nomor Telepon Belum di Inputkan!")
+    if name == "" or phone == "":
+        messagebox.showwarning("Error", "Name and phone are required!")
     else:
         with open("biodata.txt", "a") as file:
-            file.write(f"Name: {Nama_Pemesan}\nPhone: {phone}\n\n")
+            file.write(f"Name: {name}\nPhone: {phone}\n\n")
         messagebox.showinfo("Success", "Biodata saved successfully!")
 
 # Membuat jendela utama
@@ -83,26 +98,26 @@ login_button = Button(login_frame, text="Login", command=login)
 login_button.pack()
 
 # Menambahkan tombol Create Account
-create_account_button = Button(login_frame, text="Buat Akun", command=create_account)
+create_account_button = Button(login_frame, text="Create Account", command=create_account)
 create_account_button.pack()
 
 # Menambahkan frame untuk menu biodata
 biodata_frame = LabelFrame(root, text="Biodata", bg="white")
 
 # Menambahkan label dan entry untuk nama
-name_label = Label(biodata_frame, text="Nama Pemesan:", bg="white")
+name_label = Label(biodata_frame, text="Name:", bg="white")
 name_label.pack()
 name_entry = Entry(biodata_frame)
 name_entry.pack()
 
 # Menambahkan label dan entry untuk telepon
-phone_label = Label(biodata_frame, text="Nomor Telepon:", bg="white")
+phone_label = Label(biodata_frame, text="Phone:", bg="white")
 phone_label.pack()
 phone_entry = Entry(biodata_frame)
 phone_entry.pack()
 
 # Menambahkan tombol Save
-save_button = Button(biodata_frame, text="Simpan Data", command=save_biodata)
+save_button = Button(biodata_frame, text="Save", command=save_biodata)
 save_button.pack()
 
 # Menyembunyikan frame biodata
@@ -110,6 +125,9 @@ biodata_frame.pack_forget()
 
 # Dictionary untuk menyimpan akun
 accounts = {}
+
+# Memuat akun-akun yang sudah ada dari file txt
+load_accounts()
 
 # Menjalankan aplikasi
 root.mainloop()
