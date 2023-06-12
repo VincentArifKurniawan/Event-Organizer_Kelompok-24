85260f27aa0d25006e0f2bb4cc420e6ea305d9c1
Aghna SJ <santiagoaghnajunior2003@gmail.com>
2023-06-12 12:57:42 +0700

    123

--- GUI_ver2_.py
+++ GUI_ver2_.py
@@ -1,178 +1,115 @@
-from tkinter import *
+import tkinter as tk
 from tkinter import messagebox
 
 def create_account():
     username = username_entry.get()
     password = password_entry.get()
-    
-    # Mengecek apakah kolom sudah terisi semua
+
     if username == '' or password == '':
-        messagebox.showwarning("Peringatan", "Silakan isi semua kolom!")
-        return
-    
-    # Mengecek apakah username sudah terdaftar
-    if check_existing_username(username):
-        messagebox.showwarning("Peringatan", "Username sudah terdaftar!")
-        return
-    
-    # Menyimpan akun ke dalam file
-    with open("Data_Akun.txt", "a") as file:
-        file.write(f"{username}:{password}\n")
-    
-    messagebox.showinfo("Informasi", "Akun berhasil dibuat!")
+        messagebox.showwarning('Peringatan', 'Silakan isi semua kolom!')
+    elif check_duplicate_account(username, password):
+        messagebox.showwarning('Peringatan', 'Akun dengan username tersebut sudah ada!')
+    else:
+        with open('Data_Akun.txt', 'a') as file:
+            file.write(f'{username},{password}\n')
+        messagebox.showinfo('Informasi', 'Akun berhasil dibuat!')
+
+def check_duplicate_account(username, password):
+    with open('Data_Akun.txt', 'r') as file:
+        accounts = file.readlines()
+        for account in accounts:
+            saved_username, saved_password = account.strip().split(',')
+            if username == saved_username and password == saved_password:
+                return True
+        return False
 
 def login():
     username = username_entry.get()
     password = password_entry.get()
-    
-    # Mengecek apakah kolom sudah terisi semua
+
     if username == '' or password == '':
-        messagebox.showwarning("Peringatan", "Silakan isi semua kolom!")
-        return
-    
-    # Mengecek kecocokan username dan password
-    if check_existing_username(username):
-        with open("Data_Akun.txt", "r") as file:
-            lines = file.readlines()
-            for line in lines:
-                saved_username, saved_password = line.strip().split(":")
-                if saved_username == username and saved_password == password:
-                    messagebox.showinfo("Informasi", "Login berhasil!")
-                    show_biodata_menu()
-                    return
-    
-    messagebox.showwarning("Peringatan", "Username atau password salah!")
+        messagebox.showwarning('Peringatan', 'Silakan isi semua kolom!')
+    elif not check_duplicate_account(username, password):
+        messagebox.showwarning('Peringatan', 'Username atau password salah!')
+    else:
+        messagebox.showinfo('Informasi', 'Login berhasil!')
+        open_biodata_menu()
 
-def show_biodata_menu():
-    # Menghapus elemen-elemen menu login
-    username_label.destroy()
-    username_entry.destroy()
-    password_label.destroy()
-    password_entry.destroy()
-    login_button.destroy()
-    create_account_button.destroy()
-    
-    # Menambahkan judul menu biodata klien
-    title_label.config(text="Biodata Klien")
-    
-    # Menambahkan kolom input nama lengkap
-    nama_label = Label(window, text="Nama Lengkap:", font=("Helvetica", 12), bg="white")
-    nama_label.pack(pady=10)
-    
-    nama_entry = Entry(window, font=("Helvetica", 12))
-    nama_entry.pack(pady=10)
-    
-    # Menambahkan kolom input alamat tinggal
-    alamat_label = Label(window, text="Alamat Tinggal:", font=("Helvetica", 12), bg="white")
-    alamat_label.pack(pady=10)
-    
-    alamat_entry = Entry(window, font=("Helvetica", 12))
-    alamat_entry.pack(pady=10)
-    
-    # Menambahkan kolom input nomor telepon
-    telepon_label = Label(window, text="Nomor Telepon:", font=("Helvetica", 12), bg="white")
-    telepon_label.pack(pady=10)
-    
-    telepon_entry = Entry(window, font=("Helvetica", 12))
-    telepon_entry.pack(pady=10)
-    
-    # Menambahkan kolom input username
-    username_label = Label(window, text="Username:", font=("Helvetica", 12), bg="white")
-    username_label.pack(pady=10)
-    
-    username_entry = Entry(window, font=("Helvetica", 12))
-    username_entry.pack(pady=10)
-    
-    # Menambahkan kolom input password
-    password_label = Label(window, text="Password:", font=("Helvetica", 12), bg="white")
-    password_label.pack(pady=10)
-    
-    password_entry = Entry(window, show="*", font=("Helvetica", 12))
-    password_entry.pack(pady=10)
-    
-    # Menambahkan tombol Simpan
-    simpan_button = Button(window, text="Simpan", command=save_biodata, font=("Helvetica", 12))
-    simpan_button.pack(pady=10)
+def open_biodata_menu():
+    login_frame.destroy()
+    biodata_frame = tk.Frame(root)
+    biodata_frame.pack()
 
-def save_biodata():
-    nama = nama_entry.get()
-    alamat = alamat_entry.get()
-    telepon = telepon_entry.get()
-    username = username_entry.get()
-    password = password_entry.get()
-    
-    # Mengecek apakah semua kolom terisi
-    if nama == '' or alamat == '' or telepon == '' or username == '' or password == '':
-        messagebox.showwarning("Peringatan", "Silakan isi semua kolom!")
-        return
-    
-    # Menyimpan biodata ke dalam file
-    with open("Biodata_Klien.txt", "a") as file:
-        file.write(f"Nama: {nama}\n")
-        file.write(f"Alamat: {alamat}\n")
-        file.write(f"Telepon: {telepon}\n")
-        file.write(f"Username: {username}\n")
-        file.write(f"Password: {password}\n")
-    
-    messagebox.showinfo("Informasi", "Biodata berhasil disimpan!")
+    tk.Label(biodata_frame, text='Biodata Klien', font=('Arial', 16, 'bold')).pack()
 
-def check_existing_username(username):
-    with open("Data_Akun.txt", "r") as file:
-        lines = file.readlines()
-        for line in lines:
-            saved_username, _ = line.strip().split(":")
-            if saved_username == username:
-                return True
-    return False
+    tk.Label(biodata_frame, text='Nama Lengkap:').pack()
+    nama_entry = tk.Entry(biodata_frame)
+    nama_entry.pack()
 
-# Membuat jendela utama
-window = Tk()
-window.title("Event Organizer App")
-window.geometry("400x400")
+    tk.Label(biodata_frame, text='Alamat Tinggal:').pack()
+    alamat_entry = tk.Entry(biodata_frame)
+    alamat_entry.pack()
 
-# Menambahkan latar belakang gambar
-background_image = PhotoImage(file="Background.png")
-background_label = Label(window, image=background_image)
+    tk.Label(biodata_frame, text='Nomor Telepon:').pack()
+    telepon_entry = tk.Entry(biodata_frame)
+    telepon_entry.pack()
+
+    tk.Label(biodata_frame, text='Username:').pack()
+    username_entry = tk.Entry(biodata_frame)
+    username_entry.pack()
+
+    tk.Label(biodata_frame, text='Password:').pack()
+    password_entry = tk.Entry(biodata_frame, show='*')
+    password_entry.pack()
+
+    def save_biodata():
+        nama = nama_entry.get()
+        alamat = alamat_entry.get()
+        telepon = telepon_entry.get()
+        username = username_entry.get()
+        password = password_entry.get()
+
+        if nama == '' or alamat == '' or telepon == '' or username == '' or password == '':
+            messagebox.showwarning('Peringatan', 'Silakan isi semua kolom!')
+        elif not check_duplicate_account(username, password):
+            messagebox.showwarning('Peringatan', 'Username atau password tidak valid!')
+        else:
+            with open('Biodata_Klien.txt', 'a') as file:
+                file.write(f'{nama},{alamat},{telepon},{username},{password}\n')
+            messagebox.showinfo('Informasi', 'Biodata Klien berhasil disimpan!')
+
+    tk.Button(biodata_frame, text='Simpan', command=save_biodata).pack()
+
+root = tk.Tk()
+root.title('Event Organizer App')
+root.geometry('500x400')
+
+# Membuat background gambar
+background_image = tk.PhotoImage(file='Background.png')
+background_label = tk.Label(root, image=background_image)
 background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
-# Menambahkan judul
-title_label = Label(window, text="Event Organizer App", font=("Helvetica", 18, "bold"), bg="white")
-title_label.pack(pady=20)
+# Membuat frame untuk menu login
+login_frame = tk.Frame(root)
+login_frame.pack()
 
-# Menambahkan kalimat Just For You!
-subtitle_label = Label(window, text="Just For You!", font=("Helvetica", 14), bg="white")
-subtitle_label.pack(pady=10)
+# Menampilkan judul dan kalimat pada GUI
+tk.Label(login_frame, text='Event Organizer App', font=('Arial', 20, 'bold')).pack()
+tk.Label(login_frame, text='Just For You!', font=('Arial', 16)).pack()
+tk.Label(login_frame, text='Kelola Event Yang Ingin Anda Adakan\nDengan Cepat Dan Mudah, Tanpa Ribet!', font=('Arial', 12)).pack()
+tk.Label(login_frame, text='Courtesy of Kelompok 24 Kelas D Imperiale', font=('Arial', 12)).pack()
 
-# Menambahkan kalimat Kelola Event
-description_label = Label(window, text="Kelola Event Yang Ingin Anda Adakan Dengan Cepat Dan Mudah, Tanpa Ribet!", font=("Helvetica", 12), bg="white")
-description_label.pack(pady=10)
+# Menampilkan kolom input dan tombol untuk login
+tk.Label(login_frame, text='Menu Login', font=('Arial', 14, 'bold')).pack()
+tk.Label(login_frame, text='Username:').pack()
+username_entry = tk.Entry(login_frame)
+username_entry.pack()
 
-# Menambahkan kalimat Courtesy of Kelompok 24
-credit_label = Label(window, text="Courtesy of Kelompok 24 Kelas D Imperiale", font=("Helvetica", 10), bg="white")
-credit_label.pack(pady=10)
+tk.Label(login_frame, text='Password:').pack()
+password_entry = tk.Entry(login_frame, show='*')
+password_entry.pack()
 
-# Menambahkan form login
-login_frame = Frame(window, bg="white")
-login_frame.pack(pady=20)
+tk.Button(login_frame, text='Buat Akun', command=create_account).pack()
+tk.Button(login_frame, text='Login', command=login).pack()
 
-username_label = Label(login_frame, text="Username:", font=("Helvetica", 12), bg="white")
-username_label.grid(row=0, column=0, padx=10, pady=10)
-
-username_entry = Entry(login_frame, font=("Helvetica", 12))
-username_entry.grid(row=0, column=1, padx=10, pady=10)
-
-password_label = Label(login_frame, text="Password:", font=("Helvetica", 12), bg="white")
-password_label.grid(row=1, column=0, padx=10, pady=10)
-
-password_entry = Entry(login_frame, show="*", font=("Helvetica", 12))
-password_entry.grid(row=1, column=1, padx=10, pady=10)
-
-login_button = Button(login_frame, text="Login", command=login, font=("Helvetica", 12))
-login_button.grid(row=2, column=0, columnspan=2, pady=10)
-
-# Tombol membuat akun
-create_account_button = Button(window, text="Buat Akun", command=create_account, font=("Helvetica", 12))
-create_account_button.pack(pady=10)
-
-# Menjalankan aplikasi
-window.mainloop()
+root.mainloop()
